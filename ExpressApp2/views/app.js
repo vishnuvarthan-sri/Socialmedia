f6552c39bc60dba2da5d7b43393f8c3be7ef0b9a
vishnuvarthan-sri <vishnuvarthan375@gmail.com>
2020-07-11 09:26:57 +0530

    final

--- ExpressApp2/views/app.js
+++ ExpressApp2/views/app.js
@@ -24,8 +24,9 @@ var server = app.listen(5000, function () {
 // view engine setup
 app.engine('ejs', require('ejs').__express);
 app.engine('pug', require('pug').__express);
+app.engine('html', require('html').__express);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug','ejs');
+app.set('view engine', 'pug','ejs','html');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -46,12 +47,12 @@ app.get('/', function (req, res) {
 
 app.post('/login',  function (req, res) {
     if (!req.body.username || !req.body.password) {
-        return res.render('/login.html', { title: "login", message: "Please Enter both username and password" });
+        return res.sendFile('/login.html', { title: "login", message: "Please Enter both username and password" });
     }
 
     Account.findOne({ username: req.body.username }, function (error, account) {
         if (error) return console.log("error in accessing the database");
-        else if (!account) return res.render('/login.html', { title: "login", message: "Username doesnot Exists" });
+        else if (!account) return res.sendFile('/login.html', { title: "login", message: "Username doesnot Exists" });
         if (account.compare(req.body.password)) {
             req.session.user = account;
             req.session.save();
@@ -60,7 +61,7 @@ app.post('/login',  function (req, res) {
             console.log(req.session);
             res.redirect('/');
         }
-        else return res.render('/login.html', { title: "login", message: "Wrong password" });
+        else return res.sendFile('/login.html', { title: "login", message: "Wrong password" });
     });
     
 });
@@ -69,11 +70,11 @@ app.post('/login',  function (req, res) {
 // sign up a new account handler
 app.post('/signup', function (req, res) {
     if (!req.body.username || !req.body.password || !req.body.email) {
-        return res.render('/signup.html', { title: "signup", message: "Please Enter both username, password and email" });
+        return res.sendFile('/signup.html', { title: "signup", message: "Please Enter both username, password and email" });
     }
     //finding username from account database
     Account.findOne({ username: req.body.username }, function (error, account) {
-        if (account) return res.render('/signup.html', { title: "signup", message: "Username Already Exists" });
+        if (account) return res.sendFile('/signup.html', { title: "signup", message: "Username Already Exists" });
         else if (error) return console.log("error in accessing the database");
         // creating a new account
         else {
@@ -97,7 +98,7 @@ app.get('/', authenticate, function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render('/index.html', { posts: posts });
+            res.sendFile('/index.html', { posts: posts });
         }
     });
 });
