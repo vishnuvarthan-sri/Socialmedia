309c24df36a199f4b1e2f2c533c4a649bf2e8c1a
vishnuvarthan <48761045+vishnuvarthan-sri@users.noreply.github.com>
2020-09-07 21:41:42 +0530

    final

--- ExpressApp2/app.js
+++ ExpressApp2/app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var bcrypt = require('bcrypt');
 var window = require('window');
 var mongo = require('mongodb');
 var http = require("http").Server(app);
