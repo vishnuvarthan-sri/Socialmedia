36f117890b66d79ef4294eb4043c614ae1ddfd12
vishnuvarthan <48761045+vishnuvarthan-sri@users.noreply.github.com>
2020-09-08 08:52:04 +0530

    final

--- ExpressApp2/routes/Account.js
+++ ExpressApp2/routes/Account.js
@@ -1,14 +1,21 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var bcrypt = require("bcrypt");
 mongoose.connect('mongodb+srv://vishnuvarthan:thalavishnu98@cluster0.6ngdn.mongodb.net/vishnuvarthan?retryWrites=true&w=majority');
 
 
 // creating a new schema for account details
 var accountSchema = new Schema({
     uname: { type: String, required: true },
-    email: { type: String, required: true },
+    email: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (v) {
+                return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
+            }
+        }
+    },
     psw: {
         type: String,
         required: true,
